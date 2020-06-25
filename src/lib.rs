10ad9fa067b2e9369d6a8f89ef84dcb5e3c0bc49
Luke Brierley <ldbrierley@gmail.com>
2020-06-25 13:13:53 +0100

    hover and add 2

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,6 @@ type Model = i32;
 //    Update
 // ------ ------
 
-// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
 #[derive(Copy, Clone)]
 // `Msg` describes the different events you can modify state with.
 enum Msg {
@@ -35,7 +34,7 @@ enum Msg {
 // `update` describes how to handle each `Msg`.
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => *model += 1,
+        Msg::Increment => *model += 2,
     }
 }
 
@@ -43,14 +42,12 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 //     View
 // ------ ------
 
-// (Remove the line below once your `Model` become more complex.)
-#[allow(clippy::trivially_copy_pass_by_ref)]
 // `view` describes what to display.
 fn view(model: &Model) -> Node<Msg> {
     div![
         "This is a counter: ",
         C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
+        button![model, ev(Ev::PointerOver, |_| Msg::Increment),],
     ]
 }
 
